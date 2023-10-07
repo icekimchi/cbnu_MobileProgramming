f12671aa0257029cfef0195559e419b489247264
icekimchi <pjh010828@gmail.com>
2023-10-07 19:47:23 +0900

    commit

--- src/main/java/kr/easw/lesson02/controller/AWSConroller.java
+++ src/main/java/kr/easw/lesson02/controller/AWSConroller.java
@@ -1,12 +1,15 @@
 package kr.easw.lesson02.controller;
 
 import kr.easw.lesson02.model.dto.AWSKeyDto;
+import kr.easw.lesson02.model.dto.DownloadFileDto;
 import kr.easw.lesson02.service.AWSService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -43,14 +46,9 @@ public class AWSConroller {
     }
 
 
-    @PostMapping("/download")
-    private ModelAndView onDownload(@RequestParam String fileName) {
-        try {
-           // 이곳에 파일 다운로드 로직, 혹은 서비스를 통한 다운로드 호출을 구현하십시오.
-           throw new IllegalStateException("기능이 구현되지 않았습니다.");
-        } catch (Throwable e) {
-            return new ModelAndView("redirect:/server-error?errorStatus=" + e.getMessage());
-        }
+    @GetMapping("/download")
+    private ResponseEntity<byte[]> onDownload(@ModelAttribute DownloadFileDto downloadFileDto) throws IOException {
+        return awsController.getObject(downloadFileDto.getFileName());
     }
 
 }
--- src/main/java/kr/easw/lesson02/model/dto/DownloadFileDto.java
+++ src/main/java/kr/easw/lesson02/model/dto/DownloadFileDto.java
@@ -1,7 +1,9 @@
 package kr.easw.lesson02.model.dto;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+@Data
 @RequiredArgsConstructor
 public class DownloadFileDto {
     private final String fileName;
